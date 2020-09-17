6d1297684a4b9e8d1bc80f1060c2349b0237046f
SaiChandra2896 <saichandra2896@gmail.com>
2020-09-17 15:39:57 +0530

    add controller and middleware architecture

---
+++ controllers/bootcamps.js
@@ -0,0 +1,9 @@
+exports.getBootCamps = (req, res, next) => {};
+
+exports.getBootCamp = (req, res, next) => {};
+
+exports.createBootCamp = (req, res, next) => {};
+
+exports.updateBootCamp = (req, res, next) => {};
+
+exports.deleteBootCamp = (req, res, next) => {};
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// middleware
+const logger = require("./middlewares/logger");
+
+// routes
+const bootcamps = require("./routes/boocamps");
+
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+app.use(logger);
+
+// mount routes
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 

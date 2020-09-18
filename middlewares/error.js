230887bd1cacc26ddab0b2725e2b95223f3547a5
SaiChandra2896 <saichandra2896@gmail.com>
2020-09-18 14:14:01 +0530

    error handling enhanced

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,12 +4,25 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
+  //   print to console for dev
   console.log(err.stack.red);
 
   //   bad object id error or cast error
   if (err.name === "CastError") {
     const message = `Bootcamp of Id: ${err.value} not found`;
     error = new ApiError(message, 404);
+  }
+
+  //   mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = "Duplicate feild values entered";
+    error = new ApiError(message, 400);
+  }
+
+  //   mongoose validation error
+  if ((err.name = "ValidationError")) {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ApiError(message, 400);
   }
 
   res.status(error.status || 500).json({

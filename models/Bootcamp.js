39da6038b644c1189ddc37389f20e38f64179343
SaiChandra2896 <saichandra2896@gmail.com>
2020-09-18 17:31:44 +0530

    add geocoder with mapquest api

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 
+const geocoder = require("../utils/geocoder");
+
 const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -101,6 +103,25 @@ const BootcampSchema = new mongoose.Schema({
 // create bootcamp slug from name-- Look for mongoose middleware documentation pre section
 BootcampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode and create location feild
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+  //   stop address from saving to db (used it just to get location using geocoding)
+  this.address = undefined;
+
   next();
 });
 
